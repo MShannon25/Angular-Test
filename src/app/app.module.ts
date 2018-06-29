cbb224d9da9fa9b360664e37db7c4edb019e703f
Administrator <admin@g22mac01.fios-router.home>
2018-06-29 15:04:12 -0700

    lame animation added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {HttpClientModule} from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import {FormsModule} from '@angular/forms';
@@ -24,6 +25,7 @@ import { FilterPipe } from './pipes/filter.pipe';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     FormsModule,
     HttpClientModule
   ],
--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,16 +1,34 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit,  Input } from '@angular/core';
 import { People } from '../../people.model';
+import {
+	trigger,
+	state,
+	style,
+	animate,
+	transition
+  } from '@angular/animations';
 
 @Component({
 	selector: 'header-app',
 	templateUrl: './header.component.html',
-	styleUrls: ['./header.component.css']
+	styleUrls: ['./header.component.css'],
+	animations: [
+	    trigger('myAwesomeAnimation', [
+			transition(":enter", [
+				style({ opacity: 0 }),
+				animate(500, style({ opacity: 1 }))
+				])
+		])
+    ]
 })
 
 export class HeaderComponent implements OnInit {
 	@Input() person: People;
+	state: string;
 
 	constructor() { }
 
-	ngOnInit() { }
+	ngOnInit() {
+		//this.state = 'in';
+	}
 }
